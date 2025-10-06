a9a6fdfe79025c7133785173cbdf56c0f48bcacb
Mostafizur <mostafizzfuad@gmail.com>
2025-10-07 04:08:09 +0600

    feat(ui): add banner and display collections & products from database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
+import Collections from "@/components/Collections";
+import ProductList from "@/components/ProductList";
+import Image from "next/image";
+
 export default function HomePage() {
 	return (
 		<>
-			<div>Home</div>
+			<Image src="/banner.png" alt="banner" width={2000} height={1000} className="w-screen" />
+			<Collections />
+			<ProductList />
 		</>
 	);
 }

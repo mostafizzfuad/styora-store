9e8c3f69e11066fcf8dc00cf01f3b528cec08780
Mostafizur <mostafizzfuad@gmail.com>
2025-10-21 22:53:54 +0600

    feat(ui): add footer and fix responsive layout issues

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
 		<footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 bg-[#271f17] text-white text-center">
 			<aside>
 				<p>
-					Copyright © {new Date().getFullYear()} - All right reserved
+					Copyright &copy; {new Date().getFullYear()} - All right reserved
 					by Styora Store
 				</p>
 			</aside>
